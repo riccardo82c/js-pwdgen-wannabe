afe822d7568beaffb969fa2178b15d0e5e72dbb4
Riccardo Colombo <rikyregna@gmail.com>
2020-07-06 14:45:37 +0200

    init and finish LOL

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colore = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('pwd').innerHTML = nome + cognome + colore + '19';
