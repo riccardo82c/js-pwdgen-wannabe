e8a6f670a02f58618defc4714f3acf92b3d02d6b
Riccardo Colombo <rikyregna@gmail.com>
2020-07-06 19:38:17 +0200

    getElementsByTag[n]

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-var nome = prompt('Inserisci il tuo nome');
-var cognome = prompt('Inserisci il tuo cognome');
-var colore = prompt('Inserisci il tuo colore preferito');
+var nome = prompt("Inserisci il tuo nome");
+var cognome = prompt("Inserisci il tuo cognome");
+var colore = prompt("Inserisci il tuo colore preferito");
 var secretNumber = 19;
 
-document.getElementById('pwd').innerHTML = 'La tua password è: ' + nome + cognome + colore + secretNumber;
+document.getElementsByTagName("span")[0].innerHTML =
+  "La tua password è: " + nome + cognome + colore + secretNumber;
+
+/* document.getElementById("pwd").innerHTML =
+  "La tua password è: " + nome + cognome + colore + secretNumber; */
