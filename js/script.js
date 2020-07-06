1c5072575bd62f2f30b4bcf9e6ffcaad36f2d4d0
Riccardo Colombo <rikyregna@gmail.com>
2020-07-06 14:57:51 +0200

    add some style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 var colore = prompt('Inserisci il tuo colore preferito');
+var secretNumber = 19;
 
-document.getElementById('pwd').innerHTML = nome + cognome + colore + '19';
+document.getElementById('pwd').innerHTML = 'La tua password è: ' + nome + cognome + colore + secretNumber;
